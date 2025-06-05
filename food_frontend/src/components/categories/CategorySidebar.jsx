cb2fbdab10457da490e48a56d9555784d58b131c
Арслан <ars03082004@gmail.com>
2025-06-05 16:52:59 +0300

    commit 04

--- food_frontend/src/components/categories/CategorySidebar.jsx
+++ food_frontend/src/components/categories/CategorySidebar.jsx
@@ -1,26 +1,44 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
 import { Link } from 'react-router-dom';
+import styles from './CategorySidebar.module.css';
 
 export default function CategorySidebar() {
   const [categories, setCategories] = useState([]);
+  const [isOpen, setIsOpen] = useState(window.innerWidth > 768);
 
   useEffect(() => {
     api.get('/categories/')
       .then(res => setCategories(res.data))
       .catch(err => console.error('Ошибка загрузки категорий:', err));
+
+    const handleResize = () => {
+      setIsOpen(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="p-3 border rounded bg-light">
-      <h5>Категории</h5>
-      <ul className="list-unstyled">
+    <div className={styles.sidebar}>
+      <div className={styles.title} onClick={toggleSidebar}>
+        <h5>Категории</h5>
+        <button className={styles.toggleButton} type="button" aria-label="Переключить категории">
+          <span className={`${styles.toggleIcon} ${isOpen ? styles.open : ''}`}></span>
+        </button>
+      </div>
+      <ul className={`${styles.categoryList} ${isOpen ? styles.open : ''}`}>
         {categories.map(category => (
-          <li key={category.slug}>
+          <li key={category.slug} className={styles.categoryItem}>
             <Link 
               to={`/products/category/${category.slug}/`}
               state={{ categoryName: category.name }}
-              className="text-decoration-none"
+              className={styles.categoryLink}
             >
               {category.name}
             </Link>
--- food_frontend/src/pages/Home.jsx
+++ food_frontend/src/pages/Home.jsx
@@ -5,13 +5,14 @@ export default function Home() {
   return (
     <div className="container-fluid mt-4">
       <div className="row">
-        
-        <div className="col-md-2">
-          <CategorySidebar />
+        <div className="col-md-3 col-lg-2 mb-4 mb-md-0">
+          <div className="sticky-top" style={{ top: '1rem', zIndex: 100 }}>
+            <CategorySidebar />
+          </div>
         </div>
-        <div className="col-md-10">
+        <div className="col-md-9 col-lg-10">
           <ProductList />
-        </div>       
+        </div>
       </div>
     </div>
   );
