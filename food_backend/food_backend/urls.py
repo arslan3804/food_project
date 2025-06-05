37dc833b9d036a1cd624e613424be0f38f20a721
Арслан <ars03082004@gmail.com>
2025-06-05 21:09:40 +0300

    commit 06

--- food_backend/food_backend/urls.py
+++ food_backend/food_backend/urls.py
@@ -18,6 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
@@ -25,3 +30,25 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Food API",
+      default_version='v1',
+      description="Документация для приложения api проекта Food Project",
+      # terms_of_service="URL страницы с пользовательским соглашением",
+      contact=openapi.Contact(email="arslan384@yandex.ru"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   url(r'^swagger(?P<format>\.json|\.yaml)$', 
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+       name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+       name='schema-redoc'),
+] 
