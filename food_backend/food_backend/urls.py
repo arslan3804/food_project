761d012e8e0bcc72dec9ba289b456a086adfdba7
Арслан <ars03082004@gmail.com>
2025-06-06 12:18:13 +0300

    commit 17

--- food_backend/food_backend/urls.py
+++ food_backend/food_backend/urls.py
@@ -22,11 +22,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls import url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-]
-
 schema_view = get_schema_view(
    openapi.Info(
       title="Food API",
@@ -40,13 +35,15 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns += [
-   url(r'^api/swagger(?P<format>\.json|\.yaml)$', 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', 
        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
-       name='schema-swagger-ui'),
-   url(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
-       name='schema-redoc'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+        name='schema-redoc'),
+    path('', include('api.urls')),
 ]
 
 if settings.DEBUG:
